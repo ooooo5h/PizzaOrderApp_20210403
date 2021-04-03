29b07659df8d71da28ec1acb96df44e46415ea8d
전은형 <ooooo5h91@gmail.com>
2021-04-03 15:02:53 +0900

    가게 상세화면을 추가했음  - BaseActivity 상속받는 구조의 코드

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210403/ViewStoreDetailActivity.kt
@@ -0,0 +1,23 @@
+package com.neppplus.pizzaorderapp_20210403
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
