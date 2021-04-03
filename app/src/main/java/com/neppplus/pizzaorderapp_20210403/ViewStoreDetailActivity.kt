d159849e5cb87f6d0b902b3f4c80ae97b7e9fbd3
전은형 <ooooo5h91@gmail.com>
2021-04-03 15:47:24 +0900

    전화 연결 + TedPermission 작업 진행 완료

--- app/src/main/java/com/neppplus/pizzaorderapp_20210403/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210403/ViewStoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package com.neppplus.pizzaorderapp_20210403
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.neppplus.pizzaorderapp_20210403.datas.Store
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -17,7 +23,30 @@ class ViewStoreDetailActivity : BaseActivity() {
         setValues()
     }
     override fun setupEvents() {
-//        버튼 클릭
+//        버튼 클릭이벤트 넣어주기
+        callBtn.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mStore.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "전화 연결 불가", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("[설정]에서 전화 권한을 허용해주세요")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
 
     }
 
@@ -27,7 +56,6 @@ class ViewStoreDetailActivity : BaseActivity() {
         mStore = intent.getSerializableExtra("store") as Store
 
         Glide.with(mContext).load(mStore.logoURL).into(logoImg)
-
         storeNameTxt.text = mStore.name
         phoneNumTxt.text = mStore.phoneNum
 
