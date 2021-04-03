7ef185db09fc5d3674f3c3e9824b825bd00c0ef0
전은형 <ooooo5h91@gmail.com>
2021-04-03 15:55:58 +0900

    닉네임 수정 화면으로 이동

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210403/EditNicknameActivity.kt
@@ -0,0 +1,24 @@
+package com.neppplus.pizzaorderapp_20210403
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
--- app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/MyProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.pizzaorderapp_20210403.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.pizzaorderapp_20210403.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
 
@@ -15,12 +17,15 @@ class MyProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_my_profile,container, false)
-
-
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        editNicknameBtn.setOnClickListener {
+
+//            val myIntent = Intent(activity, ??)
+        }
 
 
     }
